a18b46a9a057595fa0bcba6f4365747894fcc603
brooke <brookejones406@gmail.com>
2021-10-20 10:08:53 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-//Use Jest for running the unit tests 
-
-//Use Inquirer for collecting input from the user. The application will be invoked by entering node index.js
-
-//The application must include Employee, Manager, Engineer, and Intern classes. The tests for these classes (in the _tests_ directory) must ALL pass.
 const path = require("path");
 const inquirer = require("inquirer");
 const { generateEngineer, generateIntern, generateManager } = require('path/to/file')
